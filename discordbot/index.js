1327123b153d216d493de25dfb386154073de48d
Blaze8834 <76134241+Blaze8834@users.noreply.github.com>
2020-12-26 08:00:32 -0500

    Update index.js

--- discordbot/index.js
+++ discordbot/index.js
@@ -1,1 +1,9 @@
 const Discord = require('discord.js')
+const client = new discord.Client()
+const config = require('./config.json')
+
+client.on('ready' , () => {
+  console.log('Client is ready!')
+})
+
+client.login(config.token)
