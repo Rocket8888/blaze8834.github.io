afda3c8f578b3a3281f63a75159b4a53545af0f6
Blaze8834 <76134241+Blaze8834@users.noreply.github.com>
2020-12-26 11:00:33 -0500

    Delete index.js

--- discordbot/index.js
+++
@@ -1,9 +0,0 @@
-const Discord = require('discord.js')
-const client = new discord.Client()
-const config = require('./config.json')
-
-client.on('ready' , () => {
-  console.log('Client is ready!')
-})
-
-client.login(config.token)
